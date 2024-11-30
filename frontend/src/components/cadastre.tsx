aef03d2831aa292e7734edfc4e722def7eeaff67
Cauã Vinicius Gomes Monteiro <cauavinicius.contato@gmail.com>
2024-11-30 04:40:25 -0300

    fix: fix wrong content

--- frontend/src/components/cadastre.tsx
+++ frontend/src/components/cadastre.tsx
@@ -4,8 +4,8 @@ export function Cadastre() {
   return (
     <div className="">
       <h1 className="text-[#003f25] text-[2vh] font-semibold text-[poppins] text-center">
-        Trasforme lixo em luxo com <br />
-        nossos eco-descontos
+      Transforme o amanhã, <br />
+      reciclando hoje.
       </h1>
       <Link href={"/register"} className="mt-[2vh] flex items-center justify-center">
         <button className="bg-[#003f25] w-[23vh] font-semibold text-white rounded-lg h-[5vh]">Cadastre-se</button>
